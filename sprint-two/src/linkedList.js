c36aca04d048167f5a6799eb24b5b5685c5f16dc
Hack Reactor Students <mamarildon@gmail.com>
2014-04-16 23:15:20 -0700

    need to work on contains

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,24 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(!list.tail){
+      list.tail = makeNode(value);
+    }
+    list.tail.next = makeNode(value);
+    list.tail = list.tail.next;
+    if(!list.head){
+      list.head=list.tail;
+    }
+
   };
 
   list.removeHead = function(){
+    list.head=list.head.next;
+
   };
 
   list.contains = function(target, node){
+
   };
 
   return list;
