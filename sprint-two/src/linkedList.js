e0316c65a4c01ca9dc01cc3ba6afed81a742f416
Don Mamaril <mamarildon@gmail.com>
2014-04-17 11:14:07 -0700

    linked lists complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,63 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+  	var node = makeNode(value);
+	if (!list.head) {
+  		list.head = node;
+  	}
+  	
+  	if (list.head === list.tail) {
+  		list.head.next = node;
+  	}
+
+  	if (list.tail) {
+  		list.tail.next = node;
+  	}
+
+  	list.tail = node;	
   };
 
   list.removeHead = function(){
+  	list.head = list.head.next;
   };
 
   list.contains = function(target, node){
+ 	if (node) {
+ 		if (node.value === target) {
+ 			return true;
+ 		} else if (node.next === null) {
+ 			return false;
+ 		} else {
+ 			return list.contains(target, list.head.next);
+ 		}
+ 	} else {
+ 		return list.contains(target, list.head)
+ 	}
   };
+
+
+
+
+
+  	// if (node !== list.tail) {
+  	// 	if (node.value === target) {
+  	// 		return true;
+  	// 	} else {
+  	// 		list.contains(target, list.head.next);
+  	// 	}
+  	// } else {
+  	// 	return false;
+  	// }
+
+
+
+	// if (node.value === target) {
+	// 	return true;
+	// } else if (node === list.tail) {
+	// 	return false;
+	// } else {
+	// 	list.contains(target, list.head.next);
+	// }
 
   return list;
 };
