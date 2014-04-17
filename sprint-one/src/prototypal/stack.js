6cd091b35eddca4e5324ee0572b5573498c4181c
Hack Reactor Students <mamarildon@gmail.com>
2014-04-16 20:58:19 -0700

    Completed Sprint one

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,29 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var makeStack = function(){
+  var instance = Object.create(stackMethods);
+  // //_(instance).extend(stackMethods); // extends ALL instances with stackMETHODS
+  // _.extend(instance, stackMethods);
+  instance._storage = {};
+  instance._size = 0;
+
+  return instance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+stackMethods.pop = function(){
+  if (this._size) {
+    this._size--;
+    var result = this._storage[this._size];
+    delete this._storage[this._size];
+    return result;
+  }
+};
+
+stackMethods.size = function(){
+  return this._size;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,24 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+
+  this._storage = {};
+  this._size = 0;
+
+};
+
+Stack.prototype.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+Stack.prototype.pop = function(){
+  if (this._size) {
+    this._size--;
+    var result = this._storage[this._size];
+    delete this._storage[this._size];
+    return result;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this._size;
 };
