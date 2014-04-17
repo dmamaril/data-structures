fabd8b358cbc8c247edb030455e4afc1ca2aa9af
Hack Reactor Students <mamarildon@gmail.com>
2014-04-16 20:57:20 -0700

    Completed Sprint 01

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,31 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+var makeStack = function(){
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance._storage = {};
+  instance._size = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+
+  };
+
+  instance.size = stackMethods.size;
+
+  return instance;
 };
-  
-var stackMethods = {};
+
+
+var stackMethods = {
+instance.push = function(value){
+    storage[size] = value;
+    size++;
+  };
+
+  instance.pop = function(){
+    if (size) {
+      size--;
+      var result = storage[size];
+      delete storage[size];
+      return result;
+    }
+};
